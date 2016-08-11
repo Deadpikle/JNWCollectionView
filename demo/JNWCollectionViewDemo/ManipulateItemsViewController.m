263aa91953618b5f0317ef35d3383c17a63bcf6b
Deadpikle <mikebobby@sbcglobal.net>
2016-08-11 09:29:30 -0400

    Add listview to Manipulate Items demo

--- demo/JNWCollectionViewDemo/ManipulateItemsViewController.m
+++ demo/JNWCollectionViewDemo/ManipulateItemsViewController.m
@@ -8,12 +8,16 @@
 
 #import "ManipulateItemsViewController.h"
 
+#import "ListCell.h"
 #import "GridCell.h"
 
 @interface ManipulateItemsViewController()
 
 @property (nonatomic, weak) IBOutlet JNWCollectionView *collectionView;
+@property (nonatomic, weak) IBOutlet JNWCollectionView *listView;
 @property (nonatomic, strong) NSMutableArray *items;
+@property (nonatomic, strong) NSMutableArray *listItems;
+@property NSUInteger listItemCounter;
 
 - (IBAction)addItem:(id)sender;
 - (IBAction)removeItem:(id)sender;
@@ -22,6 +26,7 @@
 @end
 
 static NSString * const identifier = @"CELL";
+static NSString * const listCellIdentifier = @"LISTCELL";
 
 @implementation ManipulateItemsViewController
 
@@ -30,9 +35,12 @@ static NSString * const identifier = @"CELL";
 }
 
 - (void)awakeFromNib {
+	self.listItemCounter = 1;
 	self.items = [NSMutableArray array];
+	self.listItems = [NSMutableArray array];
 	for (NSUInteger i = 0; i < 5; i++) {
 		[self.items addObject:[self generateSingleImage]];
+		[self.listItems addObject:[NSString stringWithFormat:@"%lu", (unsigned long)self.listItemCounter++]];
 	}
 	
 	JNWCollectionViewGridLayout *gridLayout = [[JNWCollectionViewGridLayout alloc] init];
@@ -43,10 +51,19 @@ static NSString * const identifier = @"CELL";
 	self.collectionView.delegate = self;
 	self.collectionView.dataSource = self;
 	self.collectionView.animatesSelection = NO; // (this is the default option)
-	
 	[self.collectionView registerClass:GridCell.class forCellWithReuseIdentifier:identifier];
 	
+	JNWCollectionViewListLayout *listLayout = [[JNWCollectionViewListLayout alloc] init];
+	listLayout.rowHeight = 44.f;
+	//layout.delegate = self;
+	self.listView.collectionViewLayout = listLayout;
+	self.listView.delegate = self;
+	self.listView.dataSource = self;
+	self.listView.animatesSelection = NO;
+	[self.listView registerClass:ListCell.class forCellWithReuseIdentifier:listCellIdentifier];
+	
 	[self.collectionView reloadData];
+	[self.listView reloadData];
 }
 
 #pragma mark JNWCollectionView Delegate
@@ -54,9 +71,16 @@ static NSString * const identifier = @"CELL";
 #pragma mark Data source
 
 - (JNWCollectionViewCell *)collectionView:(JNWCollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
-	GridCell *cell = (GridCell *)[collectionView dequeueReusableCellWithIdentifier:identifier];
-	cell.image = self.items[indexPath.jnw_item];
-	return cell;
+	if (collectionView == self.collectionView) {
+		GridCell *cell = (GridCell *)[collectionView dequeueReusableCellWithIdentifier:identifier];
+		cell.image = self.items[indexPath.jnw_item];
+		return cell;
+	}
+	else {
+		ListCell *cell = (ListCell *)[collectionView dequeueReusableCellWithIdentifier:listCellIdentifier];
+		cell.cellLabelText = self.listItems[indexPath.jnw_item];
+		return cell;
+	}
 }
 
 - (NSInteger)numberOfSectionsInCollectionView:(JNWCollectionView *)collectionView {
@@ -64,7 +88,12 @@ static NSString * const identifier = @"CELL";
 }
 
 - (NSUInteger)collectionView:(JNWCollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-	return self.items.count;
+	if (collectionView == self.collectionView) {
+		return self.items.count;
+	}
+	else {
+		return self.listItems.count;
+	}
 }
 
 - (CGSize)sizeForItemInCollectionView:(JNWCollectionView *)collectionView {
@@ -102,12 +131,19 @@ static NSString * const identifier = @"CELL";
 - (IBAction)addItem:(id)sender {
 	[self.items insertObject:[self generateSingleImage] atIndex:0];
 	[self.collectionView insertItemsAtIndexPaths:@[[NSIndexPath jnw_indexPathForItem:0 inSection:0]]];
+	
+	[self.listItems insertObject:[NSString stringWithFormat:@"%lu", (unsigned long)self.listItemCounter++] atIndex:0];
+	[self.listView insertItemsAtIndexPaths:@[[NSIndexPath jnw_indexPathForItem:0 inSection:0]]];
 }
 
 - (IBAction)removeItem:(id)sender {
 	if (self.items.count) {
 		[self.items removeObjectAtIndex:0];
 		[self.collectionView deleteItemsAtIndexPaths:@[[NSIndexPath jnw_indexPathForItem:0 inSection:0]]];
+	}
+	if (self.listItems.count) {
+		[self.listItems removeObjectAtIndex:0];
+		[self.listView deleteItemsAtIndexPaths:@[[NSIndexPath jnw_indexPathForItem:0 inSection:0]]];
 	}
 }
 
@@ -119,6 +155,14 @@ static NSString * const identifier = @"CELL";
 		}
 		[self.items removeAllObjects];
 		[self.collectionView deleteItemsAtIndexPaths:indexPaths];
+	}
+	if (self.listItems.count) {
+		NSMutableArray *indexPaths = [NSMutableArray array];
+		for (NSUInteger i = 0; i < self.listItems.count; i++) {
+			[indexPaths addObject:[NSIndexPath jnw_indexPathForItem:i inSection:0]];
+		}
+		[self.listItems removeAllObjects];
+		[self.listView deleteItemsAtIndexPaths:indexPaths];
 	}
 }
 @end
